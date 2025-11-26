d9aabe0ae95ee66b0cffb5958d9ecf0e9ba28473
ajayxsaini <153013883+ajayxsaini@users.noreply.github.com>
2025-11-26 07:53:38 +0000

    made SpeechBox component

---
+++ src/components/SpeechBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SpeechBox = () => {
+  return (
+    <div className='flex flex-column items-center justify-center w-screen bg-amber-50 h-[20vh] '>
+      <div className='bg-white cursor-alias'>
+        <input type="text"/>
+        <label htmlFor=""></label>
+      </div>
+    </div>
+  )
+}
+
+export default SpeechBox
