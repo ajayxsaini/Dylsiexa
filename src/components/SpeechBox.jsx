d983ab0cd7ceecc0e80909d64ebdc39469998b66
ajayxsaini <153013883+ajayxsaini@users.noreply.github.com>
2025-11-29 08:15:42 +0000

    added a feature

--- src/components/SpeechBox.jsx
+++ src/components/SpeechBox.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SpeechBox = () => {
+
+  const[active, setActive] = useState(true)
+
   const text = "Hi, now this is how we do it and if the text will increase then what will happen.ddddd ddddddddd dddd dddddd dddd ddd ddd ddd ddd ddd dddd ddddd dddddd ddddd dddd dddd ddd ddd ddd ddd dd ddd dddddd";
 
   // Split the text into words
@@ -17,11 +20,18 @@ const SpeechBox = () => {
           ))}
         </div>
       </div>
-
-      <div className='bg-gray-950 rounded-full text-white'>
-        <button className=' p-4 '>
-          Speak
-        </button>
+      <div className='flex gap-5 mt-2'>
+        <div onClick={() => setActive(!active)} className={`rounded-full text-white  ${active ? "bg-green-600 hover:bg-green-700" 
+                 : "bg-red-600 hover:bg-red-700"}`}>
+          <button className=' p-4 '>
+            {active ? "Speak" : "Stop"}
+          </button>
+        </div>
+        <div className='bg-purple-400 rounded-full text-white'>
+          <button className='p-4'>
+            Next 
+          </button>
+        </div>
       </div>
     </div>
   );
